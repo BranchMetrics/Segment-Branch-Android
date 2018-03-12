1751430bce113eb3de6a7f42f6063bf2ba87f1d8
sojan <sojan@branch.io>
2018-03-12 15:20:07 -0700

    Adding v2 event support

---
+++ segment-branch-bundle/src/main/java/com/segment/analytics/android/integrations/branch/BranchIntegration.java
@@ -0,0 +1,231 @@
+package com.segment.analytics.android.integrations.branch;
+
+import android.content.Context;
+import android.text.TextUtils;
+
+import com.segment.analytics.Analytics;
+import com.segment.analytics.ValueMap;
+import com.segment.analytics.integrations.IdentifyPayload;
+import com.segment.analytics.integrations.Integration;
+import com.segment.analytics.integrations.Logger;
+import com.segment.analytics.integrations.ScreenPayload;
+import com.segment.analytics.integrations.TrackPayload;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Iterator;
+
+import io.branch.indexing.BranchUniversalObject;
+import io.branch.referral.Branch;
+import io.branch.referral.Defines;
+import io.branch.referral.util.BRANCH_STANDARD_EVENT;
+import io.branch.referral.util.BranchEvent;
+import io.branch.referral.util.CurrencyType;
+
+/**
+ * @author sahilverma
+ * @date 2015-11-05
+ */
+public class BranchIntegration extends Integration<Branch> {
+    
+    private static final String BRANCH = "Branch Metrics";
+    private static final String VIEWED_SCREEN = "viewed_screen_";
+    private static final String TAG = "Seg-Branch-Integration";
+    final Logger mLogger;
+    final Branch mBranch; /* global instance */
+    final String mBranch_Key;
+    final BranchUtil branchUtil = new BranchUtil();
+    private static Context appContext;
+    
+    
+    // 03/09/18 PRS :This methods is called asynchronously. Segment call this method after completing few network tasks.
+    public static final Factory FACTORY = new Factory() {
+        @Override
+        public Integration<?> create(ValueMap settings, Analytics analytics) {
+            appContext = analytics.getApplication().getApplicationContext();
+            Logger logger = analytics.logger(BRANCH);
+            String branch_key = settings.getString("branch_key");
+            Branch branch = Branch.getAutoInstance(analytics.getApplication());
+            Branch.enableLogging();
+            // 03/09/18 PRS : Since this method is called async way, Branch will not be able to hook on the life cycle
+            // events for the initial activity. Initialising Branch here without depending on lifecycle method
+            try {
+                Method method = branch.getClass().getDeclaredMethod("registerAppReInit");
+                method.setAccessible(true);
+                method.invoke(branch);
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+            return new BranchIntegration(branch, logger, branch_key); // still have reference to Branch Key.
+        }
+        
+        @Override
+        public String key() {
+            return BRANCH;
+        }
+    };
+    
+    //region BranchIntegration creator
+    public BranchIntegration(Branch branch, Logger logger, String branch_key) {
+        this.mLogger = logger;
+        this.mBranch = branch;
+        this.mBranch_Key = branch_key;
+    }
+    //endregion
+    
+    //region Analytics Maps
+    @Override
+    public void identify(IdentifyPayload identifyPayload) {
+        super.identify(identifyPayload);
+        String identity = identifyPayload.userId();
+        mLogger.verbose("BranchSDK#setIdentity(%s)", identity);
+        mBranch.setIdentity(identity);
+    }
+    
+    @Override
+    public void track(TrackPayload track) {
+        super.track(track);
+        createBranchEventFromSegmentEvent(track.event(), track.properties().toJsonObject()).logEvent(appContext);
+        mLogger.verbose("BranchSDK#BranchEventLogged(%s, %s)", track.event(), track.properties().toJsonObject());
+    }
+    
+    @Override
+    public void screen(ScreenPayload screen) {
+        super.screen(screen);
+        BranchEvent branchEvent = new BranchEvent(BRANCH_STANDARD_EVENT.VIEW_ITEM);
+        updateBranchEventWithData(branchEvent, false, screen.properties().toJsonObject());
+        branchEvent.logEvent(appContext);
+        mLogger.verbose("BranchSDK#BranchEventLogged(%s, %s)", screen.event(), screen.properties().toJsonObject());
+    }
+    //endregion
+    
+    //region Singleton
+    @Override
+    public Branch getUnderlyingInstance() {
+        return mBranch;
+    }
+    //endregion
+    
+    
+    private BranchEvent createBranchEventFromSegmentEvent(String eventName, JSONObject payload) {
+        BranchEvent branchEvent;
+        if (branchUtil.getBranchStandardEvent(eventName) != null) {
+            branchEvent = new BranchEvent(branchUtil.getBranchStandardEvent(eventName));
+            updateBranchEventWithData(branchEvent, false, payload);
+        } else {
+            branchEvent = new BranchEvent(eventName);
+            updateBranchEventWithData(branchEvent, true, payload);
+        }
+        return branchEvent;
+    }
+    
+    private void updateBranchEventWithData(BranchEvent branchEvent, boolean isCustomEvent, JSONObject payload) {
+        io.branch.referral.BranchUtil.JsonReader reader = new io.branch.referral.BranchUtil.JsonReader(payload);
+         /* BranchEvent fields:
+            transactionID;
+            currency;
+            revenue;
+            shipping;
+            tax;
+            coupon;
+            affiliation;
+            eventDescription;
+            searchQuery;
+            */
+        
+            /* Segment event fields:
+        
+            "order_id": "50314b8e9bcf000000000000",
+            "affiliation": "Google Store",
+            "value": 30,
+            "revenue": 25,
+            "shipping": 3,
+            "tax": 2,
+            "discount": 2.5,
+            "coupon": "hasbros",
+            "currency": "USD"
+            "query"
+            "products" // An array of product objects
+         */
+        
+        // First Read all known params and add to the event
+        branchEvent.setAffiliation(reader.readOutString("affiliation"));
+        branchEvent.setCoupon(reader.readOutString("coupon"));
+        branchEvent.setRevenue(reader.readOutDouble("revenue"));
+        branchEvent.setSearchQuery(reader.readOutString("query"));
+        branchEvent.setShipping(reader.readOutDouble("shipping"));
+        branchEvent.setTax(reader.readOutDouble("tax"));
+        branchEvent.setTransactionID(reader.readOutString("order_id"));
+        String currencyStr = reader.readOutString("currency");
+        if (!TextUtils.isEmpty(currencyStr)) {
+            branchEvent.setCurrency(CurrencyType.getValue(currencyStr));
+        }
+        
+        JSONArray prodArray = reader.readOutJsonArray("products");
+        JSONObject customParams = reader.getJsonObject();
+        convertToBranchKeys(customParams);
+        if (prodArray != null) {
+            for (int i = 0; i < prodArray.length(); i++) {
+                JSONObject prod = prodArray.optJSONObject(i);
+                convertToBranchKeys(prod);
+                if (prod != null) {
+                    // Merge all custom data to the event to the product
+                    for (Iterator<String> it = customParams.keys(); it.hasNext(); ) {
+                        String key = it.next();
+                        try {
+                            prod.putOpt(key, customParams.optString(key));
+                        } catch (JSONException ignore) {
+                        }
+                    }
+                    BranchUniversalObject buo = BranchUniversalObject.createInstance(prod);
+                    if (buo.getContentMetadata().productCategory == null && !TextUtils.isEmpty(prod.optString(Defines.Jsonkey.ProductCategory.getKey()))) {
+                        buo.getContentMetadata().addCustomMetadata("category", prod.optString(Defines.Jsonkey.ProductCategory.getKey()));
+                    }
+                    if (buo.getContentMetadata().condition == null && !TextUtils.isEmpty(prod.optString(Defines.Jsonkey.Condition.getKey()))) {
+                        buo.getContentMetadata().addCustomMetadata("condition", prod.optString(Defines.Jsonkey.Condition.getKey()));
+                    }
+                    if (isCustomEvent) {
+                        branchEvent.addCustomDataProperty(prod.optString("id"), buo.convertToJson().toString());
+                    } else {
+                        branchEvent.addContentItems(buo);
+                    }
+                }
+            }
+        } else {
+            for (Iterator<String> it = customParams.keys(); it.hasNext(); ) {
+                String key = it.next();
+                branchEvent.addCustomDataProperty(key, customParams.optString(key));
+            }
+        }
+    }
+    
+    private void convertToBranchKeys(JSONObject object) {
+        replaceKey(object, "sku", Defines.Jsonkey.SKU.getKey());
+        replaceKey(object, "price", Defines.Jsonkey.Price.getKey());
+        replaceKey(object, "name", Defines.Jsonkey.ProductName.getKey());
+        replaceKey(object, "category", Defines.Jsonkey.ProductCategory.getKey());
+        replaceKey(object, "title", Defines.Jsonkey.ContentTitle.getKey());
+        replaceKey(object, "brand", Defines.Jsonkey.ProductBrand.getKey());
+        replaceKey(object, "rating", Defines.Jsonkey.Rating.getKey());
+        replaceKey(object, "condition", Defines.Jsonkey.Condition.getKey());
+    }
+    
+    private void replaceKey(JSONObject object, String originalKey, String branchKey) {
+        try {
+            String val = object.optString(originalKey);
+            if (!TextUtils.isEmpty(val)) {
+                object.remove(originalKey);
+                object.put(branchKey, val);
+            }
+        } catch (JSONException ignore) {
+        }
+    }
+}
---
+++ segment-branch-testbed/src/main/java/io/branch/segment_branch_testbed/MainActivity.java
@@ -0,0 +1,75 @@
+package io.branch.segment_branch_testbed;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+
+import com.segment.analytics.Analytics;
+import com.segment.analytics.Properties;
+import com.segment.analytics.android.integrations.branch.BranchIntegration;
+
+import java.util.UUID;
+
+public class MainActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Analytics analytics = new Analytics.Builder(MainActivity.this, getString(R.string.segment_write_key))
+                .use(BranchIntegration.FACTORY)
+                .build();
+        
+        
+        findViewById(R.id.cmdIdentifyUser).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                analytics.identify(UUID.randomUUID().toString());
+            }
+        });
+        
+        findViewById(R.id.cmdTrackEvent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String eventName = (String) ((Spinner) findViewById(R.id.event_name_spinner)).getSelectedItem();
+                Properties properties = new Properties()
+                        .putCategory("my category")
+                        .putCoupon("my coupon")
+                        .putCurrency("USD")
+                        .putDiscount(30.00)
+                        .putName("my property name")
+                        .putOrderId("my order id")
+                        .putPath("/my_path")
+                        .putPrice(20.50)
+                        .putProductId("my_prod_id")
+                        .putReferrer("my referrer string")
+                        .putRevenue(12.9)
+                        .putShipping(2.05)
+                        .putSku("my sku")
+                        .putTax(3.0)
+                        .putTitle("my title")
+                        .putUrl("https://my_test_url")
+                        .putTotal(22.05);
+                
+                Properties.Product product1 = new Properties.Product("prod1_id", "prod1_sku", 12.00);
+                Properties.Product product2 = new Properties.Product("prod2_id", "prod2_sku", 13.00);
+                properties.putProducts(product1, product2);
+                analytics.track(eventName, properties);
+            }
+        });
+        
+        
+        findViewById(R.id.cmdTrackView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                analytics.screen(MainActivity.class.getName());
+            }
+        });
+    }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
